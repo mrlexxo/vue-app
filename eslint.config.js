5e4ad9ed2dcd79a403301bf1c8c855337655a955
mrlexxo <alexander.ogorodov@gmail.com>
2025-08-21 21:57:13 +0400

    [fix]linted

--- eslint.config.js
+++ eslint.config.js
@@ -3,18 +3,18 @@ import js from "@eslint/js";
 import prettier from "@vue/eslint-config-prettier";
 
 export default [
-    js.configs.recommended,
-    ...vue.configs["flat/recommended"],
-    prettier,
-    {
-        files: ["**/*.vue", "**/*.js"],
-        languageOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-        },
-        rules: {
-            "vue/multi-word-component-names": "off",
-            "vue/require-default-prop": "off",
-        }
+  js.configs.recommended,
+  ...vue.configs["flat/recommended"],
+  prettier,
+  {
+    files: ["**/*.vue", "**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
-]
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "vue/require-default-prop": "off",
+    },
+  },
+];
